bf54267e3899d217a813109a857d9b7d8080f957
SthefanySF <sthefanysantos0815@gmail.com>
2023-06-30 14:56:00 -0500

    campos adicionar clientes

--- app/adicionarCliente.py
+++ app/adicionarCliente.py
@@ -1,10 +1,35 @@
 import tkinter as tk
 
 class AdicionarClientes:
+
+    #def adicionarCliente():
+     #   cli = Cliente()
+
     def __init__(self, master):
         self._janela = tk.Toplevel(master)
         self._janela.title("Adicionar clientes")
         self._janela.geometry('500x500')
+
+        lbl_nome = tk.Label(self._janela, text = 'Nome: ')
+        lbl_nome.grid(row=0, column=0)
+        ent_nome = tk.Entry(self._janela )
+        ent_nome.grid(row=0, column=1)
+
+        lbl_cpf = tk.Label(self._janela, text = 'CPF: ')
+        lbl_cpf.grid(row=1, column=0)
+        ent_cpf = tk.Entry(self._janela )
+        ent_cpf.grid(row=1, column=1)
+
+        lbl_endereco = tk.Label(self._janela, text = 'Endereço: ')
+        lbl_endereco.grid(row=2, column=0)
+        ent_endereco = tk.Entry(self._janela )
+        ent_endereco.grid(row=2, column=1)
+
+        btn_salvar = tk.Button(self._janela, text='Salvar')
+        btn_salvar.grid(row=3, columnspan=2)
+
+
+        
 
         btn = tk.Button(self._janela, text='Voltar', command=self.voltar)
         btn.pack()
